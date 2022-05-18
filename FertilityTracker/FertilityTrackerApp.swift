567922896e35727688be0de5fd33a8c3f7078391
Daniel Carr <54419206+dscarr3000@users.noreply.github.com>
2022-05-17 18:30:26 -0600

    Initial Commit

---
+++ FertilityTracker/FertilityTrackerApp.swift
@@ -0,0 +1,20 @@
+//
+//  FertilityTrackerApp.swift
+//  FertilityTracker
+//
+//  Created by Daniel Carr on 5/17/22.
+//
+
+import SwiftUI
+
+@main
+struct FertilityTrackerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
